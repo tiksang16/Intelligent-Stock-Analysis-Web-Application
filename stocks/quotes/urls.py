9190fe0caeef6a47e717a40b492c6625d9d69962
Dickson Chan <dicksonchan@Dicksons-MacBook-Air.local>
2021-01-01 21:22:59 +0800

    django project new update

--- stocks/quotes/urls.py
+++ stocks/quotes/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('about.html', views.about, name="about"),
+    path('add_stock.html', views.add_stock, name="add_stock")
 ]
--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -1,4 +1,30 @@
 from django.shortcuts import render
 
+#pk_8e85210a28d7425eadcbe2bf2a7b1072
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_8e85210a28d7425eadcbe2bf2a7b1072")
+
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
+        return render(request, 'home.html', {'api': api})
+    
+    else:
+        return render(request, 'home.html', {'ticker': "Enter a Ticker Symbol Above..."})
+
+    return render(request, 'home.html', {'api': api})
+
+def about(request):
+    return render(request,'about.html',{})
+
+
+def add_stock(request):
+    
+    ticker = Stock.object.all()
+    return render(request,'add_stock.html',{'ticker':ticker})
