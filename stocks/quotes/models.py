b8982f25144dc013e6747d94a47c64ac0912c58a
Dickson Chan <dicksonchan@Dicksons-MacBook-Air.local>
2021-01-02 23:38:04 +0800

    Finished first part

--- stocks/quotes/models.py
+++ stocks/quotes/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Stock(models.Model):
     ticker = models.CharField(max_length=10)
-
-    def __str___(self):
+    def __str__(self):
+        
         return self.ticker
--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Stock
+from .forms import StockForm
+from django.contrib import messages
 
 #pk_8e85210a28d7425eadcbe2bf2a7b1072
 def home(request):
@@ -25,6 +28,35 @@ def about(request):
 
 
 def add_stock(request):
-    
-    ticker = Stock.object.all()
-    return render(request,'add_stock.html',{'ticker':ticker})
+    import requests
+    import json
+    if request.method == 'POST':
+        form = StockForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, ("Stock Has Been Added!"))
+            return redirect('add_stock')
+    else:
+        ticker = Stock.objects.all()
+        output = []
+        for ticker_item in ticker:
+            api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + str(ticker_item) + "/quote?token=pk_8e85210a28d7425eadcbe2bf2a7b1072")
+            try:
+                api = json.loads(api_request.content)
+                output.append(api)
+            except Exception as e:
+                api = "Error..."
+
+
+        return render(request,'add_stock.html',{'ticker': ticker, 'output': output})
+
+def delete(request, stock_id):
+    item = Stock.objects.get(pk=stock_id)
+    item.delete()
+    messages.success(request,("Stock Has Been Deleted!"))
+    return redirect(delete_stock)
+
+def delete_stock(request):
+    ticker = Stock.objects.all()
+    return render(request,'delete_stock.html',{'ticker': ticker})
